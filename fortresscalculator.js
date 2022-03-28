d74690ed95eefca9d3c85cee69f88a52a5e82d46
techtechnikov <74464544+techtechnikov@users.noreply.github.com>
2022-03-28 20:18:29 +0300

    Create fortresscalculator.js

---
+++ fortresscalculator.js
@@ -0,0 +1,10 @@
+function calculateСoordinates() {
+  let first_point_coordinates = [0, 0];
+  let second_point_coordinates = [0, 0];
+  
+  let first_direction = 0.0;
+  let second_direction = 0.0;
+  
+  let fortress_cords = [0, 0];
+  
+}
