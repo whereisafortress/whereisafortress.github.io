02661b86baa42396f80016215b042e99c157adf9
techtechnikov <74464544+techtechnikov@users.noreply.github.com>
2022-03-30 16:40:56 +0300

    Update fortresscalculator.js

--- fortresscalculator.js
+++ fortresscalculator.js
@@ -1,6 +1,17 @@
+function getTanFromDegrees(degrees) {
+  return Math.tan(degrees * Math.PI / 180);
+}
+
 function calculateСoordinates(ax, az, bx, bz, da, db) {
   let fx = 0;
   let fz = 0;
+  
+  let ta = getTanFromDegrees(da);
+  let tb = getTanFromDegrees(db);
+  
+  fz = (tb*ta*bx - tb*ta*ax + az*tb - bz*ta) / (tb-ta);
+  fx = (fz-bz)/tb + bx;
+  
   return [fx, fz];
 }
 
